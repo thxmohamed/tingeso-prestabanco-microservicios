1308cb5c57224c96a0e9a6b6fd8966dd500b6a77
Mohamed Al-Marzuk <mohamed.al-marzuk@usach.cl>
2024-12-04 18:53:16 -0300

    Deployment
    Part 1 - ms user (test)

--- ms-evaluation-service/src/main/java/com/tutorial/evaluation/clients/UserClient.java
+++ ms-evaluation-service/src/main/java/com/tutorial/evaluation/clients/UserClient.java
@@ -3,7 +3,6 @@ package com.tutorial.evaluation.clients;
 import com.tutorial.evaluation.model.UserModel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.cloud.openfeign.FeignClientProperties;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
